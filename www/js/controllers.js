5ed17dbd3626fc6045ba7157aac17b02bc4bc33f
Thuong Ho <thuong.t.ho@gmail.com>
2016-01-31 19:40:51 -0800

    update controller to match park and map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+
+.controller('ParksCtrl', function ($scope, $log) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,19 +10,14 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $log.info('ParksCtrl created');
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ParkDetailCtrl', function ($scope, $stateParams, $log) {
+  $log.info('ParkDetailCtrl created');
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('MapCtrl', function ($scope, $log) {
+  $log.info('MapCtrl created');
 });
